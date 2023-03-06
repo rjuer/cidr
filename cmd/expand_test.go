17d15ce24173db703faa7fd8963ab9b4cbad1206
rjuer <21335146+rjuer@users.noreply.github.com>
2023-03-06 23:26:21 +0100

    Add bug for test purposes

--- cmd/expand_test.go
+++ cmd/expand_test.go
@@ -14,7 +14,7 @@ var expandTests = []struct {
 func TestExpand(t *testing.T) {
 	for _, test := range expandTests {
 		if got, _ := Expand(test.cidr); got != test.expected {
-			t.Errorf("Output %q not equal to expected %q", got, test.expected)
+			t.Errorf("Output %t not equal to expected %q", got, test.expected)
 		}
 	}
 }
